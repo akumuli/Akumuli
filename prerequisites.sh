4520321ace38d1c5a54ef489fc74c2670cd3fb6b
Eugene Lazin <4lazin@gmail.com>
2014-10-08 19:22:18 +0400

    Trying to make shipable happy

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,8 +2,15 @@
 
 echo 'The script will install packages using apt-get.' \
      'It will ask for your sudo password.'
+     
+echo 'Trying to install boost libraries'
 sudo apt-get install libboost-dev libboost-system-dev libboost-thread-dev \
-     libboost-filesystem-dev libboost-test-dev libboost-coroutine-dev \
+     libboost-filesystem-dev libboost-test-dev 
+sudo apt-get install libboost-coroutine-dev \
      libboost-context-dev
-sudo apt-get install libgoogle-perftools-dev
+     
+echo 'Trying to install other libraries'
+sudo apt-get install libgoogle-perftools-dev libapr1
+
+echo 'Trying to install cmake'
 sudo apt-get install cmake
