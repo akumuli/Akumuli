793b3409665c2607b55eea3ffe53f2d70e1deaa2
Eugene Lazin <4lazin@gmail.com>
2014-12-16 11:06:59 +0300

    add perf test for RESP protocol
    Signed-off-by: Eugene Lazin <4lazin@gmail.com>

---
+++ src/perf_respstream.cpp
@@ -0,0 +1,65 @@
+#include "resp.h"
+#include <boost/timer.hpp>
+#include <iostream>
+#include <algorithm>
+
+const int TEST_ITERATIONS = 1000;
+const int N_TESTS = 1000;
+
+using namespace Akumuli;
+
+int main() {
+    const char* pattern = ":1234567\r\n+3.14159\r\n";
+    std::string input;
+    for (int i = 0; i < TEST_ITERATIONS/2; i++) {
+        input += pattern;
+    }
+    std::vector<double> timedeltas;
+    uint64_t intvalue;
+    Byte buffer[RESPStream::STRING_LENGTH_MAX];
+    for (int i = N_TESTS; i --> 0;) {
+        boost::timer tm;
+        MemStreamReader stream(input.data(), input.size());
+        RESPStream protocol(&stream);
+        for (int j = TEST_ITERATIONS; i --> 0;) {
+            auto type = protocol.next_type();
+            switch(type) {
+            case RESPStream::INTEGER:
+                intvalue = protocol.read_int();
+                if (intvalue != 1234567) {
+                    std::cerr << "Bad int value at " << j << std::endl;
+                    return -1;
+                }
+                break;
+            case RESPStream::STRING: {
+                    int len = protocol.read_string(buffer, sizeof(buffer));
+                    if (len != 7) {
+                        std::cerr << "Bad string value at " << j << std::endl;
+                        return -1;
+                    }
+                    char *p = buffer;
+                    double res = strtod(buffer, &p);
+                    if (abs(res - 3.14159) > 0.0001) {
+                        std::cerr << "Can't parse float at " << j << std::endl;
+                        return -1;
+                    }
+                }
+                break;
+            case RESPStream::ARRAY:
+            case RESPStream::BAD:
+            case RESPStream::BULK_STR:
+            case RESPStream::ERROR:
+            default:
+                std::cerr << "Error at " << j << std::endl;
+                return -1;
+            };
+        }
+        timedeltas.push_back(tm.elapsed());
+    }
+    double min = std::numeric_limits<double>::max();
+    for (auto t: timedeltas) {
+        min = std::min(min, t);
+    }
+    std::cout << "Parsing " << TEST_ITERATIONS << " messages in " << min << " sec." << std::endl;
+    return 0;
+}
