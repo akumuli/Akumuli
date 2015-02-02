224db5c9ebd9739e811ce39673fc6855c5d93481
Eugene Lazin <4lazin@gmail.com>
2015-02-02 11:32:17 +0300

    WIP: main executable, add string to time conversion for sliding window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "tcp_server.h"
 
 #include <iostream>
+#include <regex>
 
 #include <boost/program_options.hpp>
 #include <apr_errno.h>
@@ -47,6 +48,21 @@ void run_server(std::string path) {
     server.stop();
 }
 
+uint64_t str2unixtime(std::string t) {
+    std::regex regex("(\\d)+\\s?(min|sec|s|m)");
+    std::smatch sm;
+    std::regex_match(t, sm, regex);
+    if (sm.size() != 2) {
+        throw std::runtime_error("Bad window size");
+    }
+    std::string snum = sm[0].str();
+    auto num = boost::lexical_cast<uint64_t>(snum);
+    if (sm[1].str() == "m" || sm[1].str() == "min") {
+        return num * 60;
+    }
+    return num;
+}
+
 int main(int argc, char** argv) {
     po::options_description desc("Akumuli options");
     desc.add_options()
@@ -55,6 +71,7 @@ int main(int argc, char** argv) {
             ("create", "Create database")
             ("name", po::value<std::string>(), "Database name (create)")
             ("nvolumes", po::value<int32_t>(), "Number of volumes to create (create)")
+            ("window", po::value<std::string>(), "Window size (create)")
             ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -71,13 +88,14 @@ int main(int argc, char** argv) {
     if (vm.count("create") == 0) {
         run_server(path);
     } else {
-        if (vm.count("nvolumes") == 0 || vm.count("name") == 0) {
+        if (vm.count("nvolumes") == 0 || vm.count("name") == 0 || vm.count("window") == 0) {
             std::cout << desc << std::endl;
             return -1;
         }
         std::string name = vm["name"].as<std::string>();
         int32_t nvol = vm["nvolumes"].as<int32_t>();
-        create_db(name.c_str(), path.c_str(), nvol, 10000, 100000, 100000);  // TODO: use correct numbers
+        std::string window = vm["window"].as<std::string>();
+        create_db(name.c_str(), path.c_str(), nvol, 10000, str2unixtime(window), 100000);  // TODO: use correct numbers
     }
 
     return 0;
