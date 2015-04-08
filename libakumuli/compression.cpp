fc706d4c23c06558e0a0f5c7f7eb49902099c9a4
Eugene Lazin <4lazin@gmail.com>
2015-04-08 14:28:30 +0300

    test implemented, new chunk layout and compression algorithm

--- libakumuli/compression.cpp
+++ libakumuli/compression.cpp
@@ -238,7 +238,7 @@ int CompressionUtil::decode_chunk( ChunkHeader *header
         }
         *pbegin = pid_reader.pos();
         if (--steps == 0) {
-            return 2;
+            return 1;
         }
     }
     case 1: {
@@ -249,7 +249,7 @@ int CompressionUtil::decode_chunk( ChunkHeader *header
         }
         *pbegin = tst_reader.pos();
         if (--steps == 0) {
-            return 1;
+            return 2;
         }
     }
     case 2: {
