61d3826d46289cc226581411c94693939db4e432
Eugene Lazin <4lazin@gmail.com>
2015-04-19 23:40:28 +0300

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 echo 'The script will install packages using apt-get.' \
-     'It will ask for your sudo password.'
+     'It can ask for your sudo password.'
      
 echo 'Trying to install boost libraries'
-sudo apt-get install libboost-dev libboost-system-dev libboost-thread-dev \
+sudo apt-get install -y libboost-dev libboost-system-dev libboost-thread-dev \
      libboost-filesystem-dev libboost-test-dev 
-sudo apt-get install libboost-coroutine-dev \
-     libboost-context-dev
+sudo apt-get install -y libboost-coroutine-dev \
+     libboost-context-dev \
+     libboost-program-options-dev libboost-regex-dev
      
 echo 'Trying to install other libraries'
-sudo apt-get install libapr1-dev libaprutil1-dev
+sudo apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
+sudo apt-get install -y liblog4cxx10-dev liblog4cxx10
+sudo apt-get install -y libjemalloc-dev
+sudo apt-get install -y libsqlite3-dev
 
 echo 'Trying to install cmake'
-sudo apt-get install cmake
+sudo apt-get install -y cmake
