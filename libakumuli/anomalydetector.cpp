eb052d593031e1d66bc2ee8e3c02a181931c7bb3
Eugene Lazin <4lazin@gmail.com>
2015-08-14 14:30:48 +0300

    WIP: anomaly detection

--- libakumuli/anomalydetector.cpp
+++ libakumuli/anomalydetector.cpp
@@ -1,15 +1,32 @@
 #include "anomalydetector.h"
 #include <random>
+#include <stdexcept>
+
+#include <boost/exception/all.hpp>
 
 namespace Akumuli {
 namespace QP {
 
-HashFnFamily::HashFnFamily(int N, int K)
+HashFnFamily::HashFnFamily(uint32_t N, uint32_t K)
+    : N(N)
+    , K(K)
 {
+    // N should be odd
+    if (N % 2 == 0) {
+        std::runtime_error err("invalid argument N (should be odd)");
+        BOOST_THROW_EXCEPTION(err);
+    }
+    // K should be a power of two
+    auto mask = K-1;
+    if ((mask&K) != 0) {
+        std::runtime_error err("invalid argument K (should be a power of two)");
+        BOOST_THROW_EXCEPTION(err);
+    }
+    // Generate tables
     std::random_device randdev;
-    std::mt19937 generator(randdev);
-    std::uniform_int_distribution distribution;
-    for (int i = 0; i < N; i++) {
+    std::mt19937 generator(randdev());
+    std::uniform_int_distribution<> distribution;
+    for (uint32_t i = 0; i < N; i++) {
         std::vector<unsigned short> col;
         auto mask = K-1;
         for (int j = 0; j < 0x10000; j++) {
@@ -24,14 +41,14 @@ static uint32_t combine(uint32_t hi, uint32_t lo) {
     return (uint32_t)(2 - (int)hi + (int)lo);
 }
 
-uint32_t HashFnFamily::hash32(int ix, uint32_t key) {
+uint32_t HashFnFamily::hash32(int ix, uint32_t key) const {
     auto hi16 = key >> 16;
     auto lo16 = key & 0xFFFF;
     auto hilo = combine(hi16, lo16);
     return table_[ix][lo16] ^ table_[ix][hi16] ^ table_[ix][hilo];
 }
 
-uint32_t HashFnFamily::hash(int ix, uint64_t key) {
+uint32_t HashFnFamily::hash(int ix, uint64_t key) const {
     auto hi32 = key >> 32;
     auto lo32 = key & 0xFFFFFFFF;
     auto hilo = combine(hi32, lo32);
@@ -43,5 +60,59 @@ uint32_t HashFnFamily::hash(int ix, uint64_t key) {
     return hi32hash ^ lo32hash ^ hilohash;
 }
 
+// SketchWindow
+SketchWindow::SketchWindow(HashFnFamily const& hf)
+    : hashes_(hf)
+    , N(hf.N)
+    , K(hf.K)
+    , sum_(0.0)
+{
+    for (uint32_t i = 0u; i < N; i++) {
+        std::vector<double> row;
+        row.resize(K, 0.0);
+        tables_.push_back(std::move(row));
+    }
+}
+
+void SketchWindow::add(uint64_t id, double value) {
+    sum_ += value;
+    for (uint32_t i = 0; i < N; i++) {
+        // calculate hash from id to K
+        uint32_t hash = hashes_.hash(i, id);
+        tables_[i][hash] += value;
+    }
+}
+
+double SketchWindow::estimate(uint64_t id) const {
+    std::vector<double> results;
+    for (uint32_t i = 0u; i < N; i++) {
+        uint32_t hash = hashes_.hash(i, id);
+        double value = tables_[i][hash];
+        double estimate = (value - sum_/K)/(1. - 1./K);
+        results.push_back(estimate);
+    }
+    std::sort(results.begin(), results.end());
+    return results[N/2];
+}
+
+double SketchWindow::estimateF2() const {
+    std::vector<double> results;
+    auto f = 1./(K - 1);
+    for (uint32_t i = 0u; i < N; i++) {
+        double rowsum = std::accumulate(tables_[i].begin(), tables_[i].end(), 0.0, [](double acc, double val) {
+            return acc + val*val;
+        });
+        double res = K*f*sqrt(rowsum) - f*sum_*sum_;
+        results.push_back(res);
+    }
+    std::sort(results.begin(), results.end());
+    return results[N/2];
+}
+
+double& SketchWindow::at(int row, int col) {
+    return tables_[row][col];
+}
+
+}
 }
 
--- libakumuli/anomalydetector.h
+++ libakumuli/anomalydetector.h
@@ -1,26 +1,128 @@
 #pragma once
 #include <cinttypes>
 #include <vector>
+#include <deque>
+#include <memory>
 
 namespace Akumuli {
 namespace QP {
 
 //! Family of 4-universal hash functions
 struct HashFnFamily {
+    const uint32_t N;
+    const uint32_t K;
+    //! Tabulation based hash fn used, N tables should be generated using RNG in c-tor
     std::vector<std::vector<unsigned short>> table_;
 
     //! C-tor. N - number of different hash functions, K - number of values (should be a power of two)
-    HashFnFamily(int N, int K);
+    HashFnFamily(uint32_t N, uint32_t K);
 
     //! Calculate hash value in range [0, K)
-    uint32_t hash(int ix, uint64_t key);
+    uint32_t hash(int ix, uint64_t key) const;
 
 private:
-    uint32_t hash32(int ix, uint32_t key);
+    uint32_t hash32(int ix, uint32_t key) const;
 };
 
-struct AnomalyDetector {
+struct SketchWindow {
+    HashFnFamily const& hashes_;
+    const uint32_t N;
+    const uint32_t K;
+    double sum_;
+    std::vector<std::vector<double>> tables_;
 
+    SketchWindow(HashFnFamily const& hf);
+
+    void add(uint64_t id, double value);
+
+    //! Unbiased value estimator
+    double estimate(uint64_t id) const;
+
+    //! Second moment estimator
+    double estimateF2() const;
+
+    double& at(int row, int col);
+};
+
+
+// TODO: algorithm should be parametrized (SMA used now for simplicity)
+struct AnomalyDetector {
+    HashFnFamily hashes_;
+    const uint32_t N;
+    const uint32_t K;
+    const uint32_t DEPTH;
+    typedef std::unique_ptr<SketchWindow> PSketchWindow;
+    PSketchWindow window_;
+    std::deque<PSketchWindow> old_;
+    std::deque<PSketchWindow> forecast_;
+    std::deque<PSketchWindow> error_;
+
+    AnomalyDetector(uint32_t N, uint32_t K)
+        : hashes_(N, K)
+        , N(N)
+        , K(K)
+        , DEPTH(5)
+    {
+    }
+
+    void add(uint64_t id, double value) {
+        window_->add(id, value);
+    }
+
+    void move_sliding_window() noexcept {
+        PSketchWindow forecast = std::move(SMA());
+        if (forecast) {
+            PSketchWindow diff = std::move(calculate_error(forecast, window_));
+            // INVARIANT: foreacast_ and error_ always updated together
+            forecast_.push_back(std::move(forecast));
+            error_.push_back(std::move(diff));
+        }
+        old_.push_back(std::move(window_));
+        window_.reset(new SketchWindow(hashes_));
+
+        if (old_.size() > DEPTH) {
+            old_.pop_front();
+        }
+        if (forecast_.size() > DEPTH) {
+            forecast_.pop_front();
+            error_.pop_front();
+        }
+    }
+
+    PSketchWindow SMA() const {
+        PSketchWindow res;
+        if (old_.size() < DEPTH) {
+            // return empty response
+            return std::move(res);
+        }
+        res.reset(new SketchWindow(hashes_));
+        for (auto it = old_.size() - DEPTH; it < old_.size(); it++) {
+            for (auto row = 0u; row < N; row++) {
+                for (auto col = 0u; col < K; col++) {
+                    res->at(row, col) += old_[it]->at(row, col);
+                }
+            }
+        }
+        for (auto row = 0u; row < N; row++) {
+            for (auto col = 0u; col < K; col++) {
+                res->at(row, col) /= double(DEPTH);
+            }
+        }
+        return std::move(res);
+    }
+
+    PSketchWindow calculate_error(const PSketchWindow& forecast, const PSketchWindow& error) {
+        PSketchWindow res;
+        res.reset(new SketchWindow(hashes_));
+
+        for (auto row = 0u; row < N; row++) {
+            for (auto col = 0u; col < K; col++) {
+                res->at(row, col) = forecast->at(row, col) - error->at(row, col);
+            }
+        }
+
+        return std::move(res);
+    }
 };
 
 }
