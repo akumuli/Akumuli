6f35ddbe979cdfb37dfbd6d05623bd683593ef65
Eugene Lazin <4lazin@gmail.com>
2015-08-30 02:12:34 +0300

    WIP: SAX encoding

--- libakumuli/queryprocessor.cpp
+++ libakumuli/queryprocessor.cpp
@@ -482,6 +482,48 @@ struct AnomalyDetector : Node {
 };
 
 
+static std::tuple<double, double> mean_and_stddev(double* array, size_t size) {
+    if (size == 0) {
+        return std::make_tuple(NAN, NAN);
+    }
+    double sqrsum = 0;
+    double sum = 0;
+    int count = 0;
+    for (size_t i = 0; i < size; i++) {
+        sqrsum += array[i] * array[i];
+        sum += array[i];
+        count += 1;
+    }
+    double stddev;
+    if (size > 1) {
+        stddev = sqrt((size * sqrsum - sum * sum) / (size * (size - 1)));
+    } else {
+        stddev = NAN;
+    }
+    double mean = sum / size;
+    return std::make_tuple(mean, stddev);
+}
+
+//! Z-norm series in-place
+static void znorm(double* array, size_t size, double threshold) {
+    double mean, stddev;
+    std::tie(mean, stddev) = mean_and_stddev(array, size);
+    if (stddev < threshold) {
+        for (size_t i = 0; i < size; i++) {
+            array[i] -= mean;
+        }
+    } else {
+        for (size_t i = 0; i < size; i++) {
+            array[i] = (array[i] - mean) / stddev;
+        }
+    }
+}
+
+struct SAXEncoder {
+    const int alphabet_size_;
+
+};
+
 
 //                                   //
 //         Factory methods           //
