349704e337d4cfcc15b2c8376aaf0bfe659b303a
Eugene Lazin <4lazin@gmail.com>
2015-09-13 02:21:55 +0300

    WIP: udp server

--- akumulid/udp_server.cpp
+++ akumulid/udp_server.cpp
@@ -1,3 +1,93 @@
 #include "udp_server.h"
 
+#include <thread>
+
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+
+namespace Akumuli {
+
+UdpServer::UdpServer(int nworkers, int port, std::shared_ptr<IngestionPipeline> pipeline)
+    : start_barrier_(nworkers + 1)
+    , stop_barrier_(nworkers + 1)
+    , stop_{0}
+    , port_(port)
+{
+    // Create workers
+    for (int i = 0; i < nworkers; i++) {
+        auto spout = pipeline->make_spout();
+        std::thread thread(std::bind(&UdpServer::worker, shared_from_this(), spout));
+        thread.detach();
+    }
+}
+
+
+void UdpServer::start() {
+    start_barrier_.wait();
+}
+
+
+void UdpServer::stop() {
+    stop_.store(1, std::memory_order_relaxed);
+    stop_barrier_.wait();
+}
+
+
+void UdpServer::worker(std::shared_ptr<PipelineSpout> spout, std::shared_ptr<IOBuf> iobuf) {
+    start_barrier_.wait();
+
+    int sockfd, retval;
+    sockaddr_in sa;
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        perror("socket()");
+        std::cerr << "FAILURE" << std::endl;
+        return;
+    }
+
+    // Set socket options
+    int optval = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
+        perror("setsockopt()");
+        std::cerr << "FAILURE" << std::endl;
+        return;
+    }
+
+    // Bind socket to port
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+    sa.sin_port = htons(port_);
+
+    if (bind(sockfd, (sockaddr *) &sa, sizeof(sa)) == -1) {
+        std::cerr << "FAILURE" << std::endl;
+        return;
+    }
+
+    while(!stop_.load(std::memory_order_relaxed)) {
+        retval = recvmmsg(sockfd, iobuf->msgs, NPACKETS, MSG_WAITFORONE, nullptr);
+        if (retval == -1) {
+            // TODO: better error processing
+            perror("recvmmsg()");
+            std::cerr << "FAILURE" << std::endl;
+            return;
+        }
+
+        iobuf->pps++;
+
+        for (int i = 0; i < retval; i++) {
+            // reset buffer to receive new message
+            iobuf->bps += iobuf->msgs[i].msg_len;
+            iobuf->msgs[i].msg_len = 0;
+        }
+    }
+    stop_barrier_.wait();
+}
+
+}
 
--- akumulid/udp_server.h
+++ akumulid/udp_server.h
@@ -17,11 +17,14 @@
 #pragma once
 
 #include <memory>
+#include <atomic>
 
 #include <boost/thread/barrier.hpp>
 
 #include "ingestion_pipeline.h"
 #include "logger.h"
+#include "protocolparser.h"
+
 
 namespace Akumuli {
 
@@ -31,10 +34,38 @@ namespace Akumuli {
 class UdpServer : public std::enable_shared_from_this<UdpServer>
 {
     std::shared_ptr<IngestionPipeline> pipeline_;
+    ProtocolParser parser_;
     boost::barrier start_barrier_;  //< Barrier to start worker thread
     boost::barrier stop_barrier_;   //< Barrier to stop worker thread
+    std::atomic<int> stop_;
+    int port_;
 
     Logger logger_;
+
+    static const int MSS = 2048-128;
+    static const int NPACKETS = 512;
+
+    struct IOBuf {
+        // Counters
+        std::atomic<uint64_t> pps;
+        std::atomic<uint64_t> bps;
+
+        // Packet recv structs
+        mmsghdr   msgs[NPACKETS];
+        iovec   iovecs[NPACKETS];
+        char      bufs[NPACKETS][MSS];
+
+        IOBuf() {
+            memset(this, 0, sizeof(IOBuf));
+            for (int i = 0; i < NPACKETS; i++) {
+                iovecs[i].iov_base         = bufs[i];
+                iovecs[i].iov_len          = MSS;
+                msgs[i].msg_hdr.msg_iov    = &iovecs[i];
+                msgs[i].msg_hdr.msg_iovlen = 1;
+            }
+        }
+
+    } __attribute__((aligned (64)));  // Otherwise struct will be aligned by sizeof(bufs) and this is crazy expensive
 
 public:
 
@@ -52,7 +83,7 @@ public:
     void stop();
 
 private:
-    void worker(std::shared_ptr<PipelineSpout> spout);
+    void worker(std::shared_ptr<PipelineSpout> spout, std::shared_ptr<IOBuf> iobuf);
 };
 
 }  // namespace
