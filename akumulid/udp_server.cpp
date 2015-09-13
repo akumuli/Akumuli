df63720606a2512c77fd533fe04c8df0c44cb64e
Eugene Lazin <4lazin@gmail.com>
2015-09-13 22:06:54 +0300

    WIP: UDP server

--- akumulid/udp_server.cpp
+++ akumulid/udp_server.cpp
@@ -43,49 +43,63 @@ void UdpServer::worker(std::shared_ptr<PipelineSpout> spout, std::shared_ptr<IOB
     int sockfd, retval;
     sockaddr_in sa;
 
-    // Create socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        perror("socket()");
-        std::cerr << "FAILURE" << std::endl;
-        return;
-    }
-
-    // Set socket options
-    int optval = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
-        perror("setsockopt()");
-        std::cerr << "FAILURE" << std::endl;
-        return;
-    }
-
-    // Bind socket to port
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);
-    sa.sin_port = htons(port_);
-
-    if (bind(sockfd, (sockaddr *) &sa, sizeof(sa)) == -1) {
-        std::cerr << "FAILURE" << std::endl;
-        return;
-    }
-
-    while(!stop_.load(std::memory_order_relaxed)) {
-        retval = recvmmsg(sockfd, iobuf->msgs, NPACKETS, MSG_WAITFORONE, nullptr);
-        if (retval == -1) {
-            // TODO: better error processing
-            perror("recvmmsg()");
-            std::cerr << "FAILURE" << std::endl;
-            return;
+    try {
+        // Create socket
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sockfd == -1) {
+            const char* msg = strerror(errno);
+            std::stringstream fmt;
+            fmt << "can't create socket: " << msg;
+            std::runtime_error err(fmt.str());
+            BOOST_THROW_EXCEPTION(err);
         }
 
-        iobuf->pps++;
-
-        for (int i = 0; i < retval; i++) {
-            // reset buffer to receive new message
-            iobuf->bps += iobuf->msgs[i].msg_len;
-            iobuf->msgs[i].msg_len = 0;
+        // Set socket options
+        int optval = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
+            const char* msg = strerror(errno);
+            std::stringstream fmt;
+            fmt << "can't set socket options: " << msg;
+            std::runtime_error err(fmt.str());
+            BOOST_THROW_EXCEPTION(err);
         }
+
+        // Bind socket to port
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = htonl(INADDR_ANY);
+        sa.sin_port = htons(port_);
+
+        if (bind(sockfd, (sockaddr *) &sa, sizeof(sa)) == -1) {
+            const char* msg = strerror(errno);
+            std::stringstream fmt;
+            fmt << "can't bind socket: " << msg;
+            std::runtime_error err(fmt.str());
+            BOOST_THROW_EXCEPTION(err);
+        }
+
+        while(!stop_.load(std::memory_order_relaxed)) {
+            retval = recvmmsg(sockfd, iobuf->msgs, NPACKETS, MSG_WAITFORONE, nullptr);
+            // TODO: uset timeout to wake up thread periodically
+            if (retval == -1) {
+                const char* msg = strerror(errno);
+                std::stringstream fmt;
+                fmt << "socket read error: " << msg;
+                std::runtime_error err(fmt.str());
+                BOOST_THROW_EXCEPTION(err);
+            }
+
+            iobuf->pps++;
+
+            for (int i = 0; i < retval; i++) {
+                // reset buffer to receive new message
+                iobuf->bps += iobuf->msgs[i].msg_len;
+                iobuf->msgs[i].msg_len = 0;
+            }
+        }
+    } catch(...) {
+        logger_.error() << boost::current_exception_diagnostic_information();
     }
+
     stop_barrier_.wait();
 }
 
