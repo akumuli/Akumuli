762e4ff7d601eed83d2306a44c8ad0f30e88854f
Eugene Lazin <4lazin@gmail.com>
2015-10-14 11:04:02 +0300

    Unused type definition removed

--- akumulid/server.h
+++ akumulid/server.h
@@ -4,7 +4,6 @@
 namespace Akumuli {
     //! Server interface
     struct Server {
-        typedef std::function<void()> Callback;
         virtual ~Server() = default;
         virtual void start(SignalHandler* sig_handler, int id) = 0;
     };
