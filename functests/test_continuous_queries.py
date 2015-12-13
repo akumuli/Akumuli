ca58b74a8ccc71e3976448784e180ac3ac26bb9d
Eugene Lazin <4lazin@gmail.com>
2015-12-13 18:52:58 +0300

    WIP: continuous queries test

--- functests/test_continuous_queries.py
+++ functests/test_continuous_queries.py
@@ -18,82 +18,92 @@ HOST = '127.0.0.1'
 TCPPORT = 8282
 HTTPPORT = 8181
 
+
 """
 Test plan:
-    Run two parallel processes.
-    Process 1.
+    Process 1 (reader).
+    - Start process 2 (writer).
+    - Read all data in fwd direction in range [begin, end-window].
+    Process 2 (writer).
     - Write data in range [begin, mid] in a loop.
     - Long pause.
     - Write data in range (mid, end] in a loop.
     - Exit.
-    Process 2.
-    - Read all data in fwd direction in range [begin, end-window].
 """
 
-def writer(dt, delta, nmsgs):
+def writer(dt, delta, N):
     try:
         chan = att.TCPChan(HOST, TCPPORT)
 
         # fill data in
-        print("Sending {0} messages through TCP...".format(nmsgs))
+        print("Sending {0} messages through TCP...".format(N))
         tags = {
             "tag": ['Foo'],
         }
         N1 = N/2 if N % 2 == 0 else N/2 + 1
         N2 = N/2
+        print("Generating first {0} messages...".format(N1))
         for it in att.generate_messages(dt, delta, N1, 'test', **tags):
             chan.send(it)
         time.sleep(10)
+        print("Generating last {0} messages...".format(N2))
         for it in att.generate_messages(dt + delta*N1, delta, N2, 'test', **tags):
             chan.send(it)
         time.sleep(10)
     except:
+        print("Exception in writer")
         traceback.print_exc()
         sys.exit(1)
 
 def reader(dtstart, delta, N):
-    window = att.get_window_width()
-    end = dtstart + delta*(N-1) - window
-    begin = dtstart
-    timedelta = end - begin
-    points_required = int(math.ceil((timedelta.seconds*1000000.0 + timedelta.microseconds) / (delta.seconds*1000000.0 + delta.microseconds))) + 1
-    # We need to add 1 because query will include both begin and end timestamps.
+    # Start writer process
+    wproc = multiprocessing.Process(target=writer, args=[dtstart, delta, N])
+    wproc.start()
 
-    query_params = {
-        "output": { "format":  "csv" },
-    }
-    query = att.makequery("test", begin, end, **query_params)
-    queryurl = "http://{0}:{1}".format(HOST, HTTPPORT)
-    response = urlopen(queryurl, json.dumps(query))
+    try:
+        window = att.get_window_width()
+        end = dtstart + delta*(N-1) - window
+        begin = dtstart
+        timedelta = end - begin
+        points_required = int(math.ceil((timedelta.seconds*1000000.0 + timedelta.microseconds) / 
+                                        (delta.seconds*1000000.0 + delta.microseconds))) + 1
+        query_params = {"output": { "format":  "csv" }}
+        query = att.makequery("test", begin, end, **query_params)
+        queryurl = "http://{0}:{1}".format(HOST, HTTPPORT)
+        print("Query started")
+        response = urlopen(queryurl, json.dumps(query))
 
-    exp_ts = begin
-    exp_value = 0
-    iterations = 0
+        exp_ts = begin
+        exp_value = 0
+        iterations = 0
 
-    print("Test #6 - filter by tag")
+        print("Test #1 - continuous queries")
 
-    for line in response:
-        try:
-            columns = line.split(',')
-            tagline = columns[0].strip()
-            timestamp = att.parse_timestamp(columns[1].strip())
-            value = float(columns[2].strip())
+        for line in response:
+            try:
+                columns = line.split(',')
+                tagline = columns[0].strip()
+                timestamp = att.parse_timestamp(columns[1].strip())
+                value = float(columns[2].strip())
 
-            exp_tags = 'test tag=Foo'
+                exp_tags = 'test tag=Foo'
 
-            att.check_values(exp_tags, tagline, 'ENDS', exp_ts, timestamp, exp_value*1.0, value, iterations)
+                att.check_values(exp_tags, tagline, 'ENDS', exp_ts, timestamp, exp_value*1.0, value, iterations)
 
-            exp_ts += delta
-            exp_value += 1
-            iterations += 1
-        except:
-            print("Error at line: {0}".format(line))
-            raise
+                exp_ts += delta
+                exp_value += 1
+                iterations += 1
+            except:
+                print("Error at line: {0}".format(line))
+                raise
 
-    # Check that we received all values
-    if iterations != points_required:
-        raise ValueError("Expect {0} data points, get {1} data points".format(points_required, iterations))
-    print("Test #6 passed")
+        print("Query completed")
+        # Check that we received all values
+        if iterations != points_required:
+            raise ValueError("Expect {0} data points, get {1} data points".format(points_required, iterations))
+        print("Test #1 passed")
+    finally:
+        wproc.join()
 
 def main(path, debug=False):
     if not os.path.exists(path):
@@ -117,13 +127,8 @@ def main(path, debug=False):
         delta = datetime.timedelta(milliseconds=1)
         nmsgs = 100000
 
-        wproc = multiprocessing.Process(target=writer, args=[dt, delta, nmsgs])
         rproc = multiprocessing.Process(target=reader, args=[dt, delta, nmsgs])
-
-        wproc.start()
         rproc.start()
-
-        wproc.join()
         rproc.join()
 
     except:
