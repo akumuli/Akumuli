39e51077000a9fabb75298a307a355e25aca9be3
Eugene Lazin <4lazin@gmail.com>
2016-04-11 01:25:41 -0700

    WIP: blockstore

--- libakumuli/storage_engine/blockstore.cpp
+++ libakumuli/storage_engine/blockstore.cpp
@@ -1,18 +1,70 @@
 #include "blockstore.h"
+#include "log_iface.h"
+#include "util.h"
 
 namespace Akumuli {
 namespace V2 {
 
 
+//! Represents memory block
+class Block {
+    std::weak_ptr<BlockStore> store_;
+    std::vector<uint8_t> data_;
+    LogicAddr addr_;
+public:
+    Block(std::shared_ptr<BlockStore> bs, LogicAddr addr)
+        : store_(bs)
+        , data_(Volume::BLOCK_SIZE, 0)
+        , addr_(addr)
+    {
+        // TODO: read data from datastore
+    }
+
+    const uint8_t* get_data() const {
+        return data_.data();
+    }
+
+    size_t get_size() const {
+        return data_.size();
+    }
+};
+
+
 BlockStore::BlockStore(std::string metapath, std::vector<std::string> volpaths)
     : meta_(MetaVolume::open_existing(metapath.c_str()))
 {
-    for (size_t ix = 0ul; ix < volpaths.size(); ix++) {
+    for (uint32_t ix = 0ul; ix < volpaths.size(); ix++) {
         auto volpath = volpaths.at(ix);
-        auto nblocks = meta_->get_nblocks();
+        uint32_t nblocks = 0;
+        aku_Status status = AKU_SUCCESS;
+        std::tie(status, nblocks) = meta_->get_nblocks(ix);
+        if (status != AKU_SUCCESS) {
+            Logger::msg(AKU_LOG_ERROR, std::string("Can't open blockstore, volume " +
+                                                   std::to_string(ix) + " failure: " +
+                                                   aku_error_message(status)));
+            AKU_PANIC("Can't open blockstore");
+        }
         auto uptr = Volume::open_existing(volpath.c_str(), nblocks);
         volumes_.push_back(std::move(uptr));
     }
+}
+
+std::shared_ptr<BlockStore> BlockStore::open(std::string metapath, std::vector<std::string> volpaths) {
+    return std::make_shared<BlockStore>(metapath, volpaths);
+}
+
+static uint32_t extract_gen(LogicAddr addr) {
+    return addr >> 32;
+}
+
+static BlockAddr extract_vol(LogicAddr addr) {
+    return addr & 0xFFFFFFFF;
+}
+
+std::tuple<aku_Status, std::unique_ptr<Block>> BlockStore::read_block(LogicAddr addr) {
+    auto gen = extract_gen(addr);
+    auto vol = extract_vol(addr);
+    throw "not implemented";
 }
 
 }}  // namespace
--- libakumuli/storage_engine/blockstore.h
+++ libakumuli/storage_engine/blockstore.h
@@ -4,16 +4,26 @@
 namespace Akumuli {
 namespace V2 {
 
+//! Address of the block inside storage
+typedef uint64_t LogicAddr;
+
+class Block;
 
 /** Blockstore. Contains collection of volumes.
   * Translates logic adresses into physical ones.
   */
-struct BlockStore
+class BlockStore : public std::enable_shared_from_this<BlockStore>
 {
     std::unique_ptr<MetaVolume> meta_;
-    std::vector<std::unique_ptr<Volume> volumes_;
+    std::vector<std::unique_ptr<Volume>> volumes_;
 
     BlockStore(std::string metapath, std::vector<std::string> volpaths);
+
+private:
+
+    static std::shared_ptr<BlockStore> open(std::string metapath, std::vector<std::string> volpaths);
+
+    std::tuple<aku_Status, std::unique_ptr<Block>> read_block(LogicAddr addr);
 };
 
 }}  // namespace
