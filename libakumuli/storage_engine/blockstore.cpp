7b847eadf24c07caf9fc3e30677307a853f11212
Eugene Lazin <4lazin@gmail.com>
2016-04-11 01:50:53 -0700

    WIP: BlockStore, append_block implemented

--- libakumuli/storage_engine/blockstore.cpp
+++ libakumuli/storage_engine/blockstore.cpp
@@ -12,7 +12,7 @@ class Block {
     std::vector<uint8_t> data_;
     LogicAddr addr_;
 public:
-    Block(std::shared_ptr<BlockStore> bs, LogicAddr addr, std::vector<uint8_t> data)
+    Block(std::shared_ptr<BlockStore> bs, LogicAddr addr, std::vector<uint8_t>&& data)
         : store_(bs)
         , data_(std::move(data))
         , addr_(addr)
@@ -46,10 +46,30 @@ BlockStore::BlockStore(std::string metapath, std::vector<std::string> volpaths)
         auto uptr = Volume::open_existing(volpath.c_str(), nblocks);
         volumes_.push_back(std::move(uptr));
     }
+
+    // set current volume, current volume is a volume with minimal generation
+    uint32_t minix = 0u;
+    uint32_t mingen = 0u;
+    for (size_t i = 0u; i < volumes_.size(); i++) {
+        uint32_t curr_gen;
+        aku_Status status;
+        std::tie(status, curr_gen) = meta_->get_generation(i);
+        if (status != AKU_SUCCESS) {
+            Logger::msg(AKU_LOG_ERROR, "Can't find current volume, meta-volume corrupted");
+            AKU_PANIC("Meta-volume corrupted");
+        }
+        if (mingen > curr_gen) {
+            mingen = curr_gen;
+            minix = i;
+        }
+    }
+    current_volume_ = minix;
+    current_gen_ = mingen;
 }
 
 std::shared_ptr<BlockStore> BlockStore::open(std::string metapath, std::vector<std::string> volpaths) {
-    return std::make_shared<BlockStore>(metapath, volpaths);
+    auto bs = new BlockStore(metapath, volpaths);
+    return std::shared_ptr<BlockStore>(bs);
 }
 
 static uint32_t extract_gen(LogicAddr addr) {
@@ -65,13 +85,28 @@ std::tuple<aku_Status, std::shared_ptr<Block>> BlockStore::read_block(LogicAddr 
     auto vol = extract_vol(addr);
     auto volix = gen % volumes_.size();
     std::vector<uint8_t> dest(Volume::BLOCK_SIZE, 0);
-    auto status = volumes_[volix]->read_data(vol, dest.data());
+    auto status = volumes_[volix]->read_block(vol, dest.data());
     if (status != AKU_SUCCESS) {
         return std::make_tuple(status, std::unique_ptr<Block>());
     }
     auto self = shared_from_this();
-    auto block = std::make_shared(Block(self, addr, std::move(data)));
+    auto block = std::make_shared<Block>(self, addr, std::move(dest));
     return std::make_tuple(status, std::move(block));
+}
+
+static LogicAddr make_logic(uint32_t gen, BlockAddr addr) {
+    return static_cast<uint64_t>(gen) << 32 | addr;
+}
+
+std::tuple<aku_Status, LogicAddr> BlockStore::append_block(uint8_t const* data) {
+    // TODO: add notion of Volume order and current volume
+    throw "not implemented";
+    BlockAddr block_addr;
+    aku_Status status;
+    do {
+        std::tie(status, block_addr) = volumes_.at(current_volume_)->append_block(data);
+    } while(status == AKU_EOVERFLOW);
+    return std::make_tuple(status, make_logic(current_gen_, block_addr));
 }
 
 }}  // namespace
--- libakumuli/storage_engine/blockstore.h
+++ libakumuli/storage_engine/blockstore.h
@@ -16,14 +16,24 @@ class BlockStore : public std::enable_shared_from_this<BlockStore>
 {
     std::unique_ptr<MetaVolume> meta_;
     std::vector<std::unique_ptr<Volume>> volumes_;
+    uint32_t current_volume_;
+    uint32_t current_gen_;
 
     BlockStore(std::string metapath, std::vector<std::string> volpaths);
 
-private:
-
+public:
+    /** Create BlockStore instance (can be created only on heap).
+      */
     static std::shared_ptr<BlockStore> open(std::string metapath, std::vector<std::string> volpaths);
 
+    /** Read block from blockstore
+      */
     std::tuple<aku_Status, std::shared_ptr<Block> > read_block(LogicAddr addr);
+
+    /** Add block to blockstore.
+      * @return Status and block's logic address.
+      */
+    std::tuple<aku_Status, LogicAddr> append_block(uint8_t const* data);
 };
 
 }}  // namespace
