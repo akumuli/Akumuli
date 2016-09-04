7a0f2e54152c0904814824d3e938dba2af769811
Eugene Lazin <4lazin@gmail.com>
2016-09-04 10:23:31 -0700

    Ingestion unit-test removed and code moved to column-store test suite.

--- unittests/test_column_store.cpp
+++ unittests/test_column_store.cpp
@@ -39,10 +39,149 @@ static AkumuliInitializer initializer;
 using namespace Akumuli;
 using namespace Akumuli::StorageEngine;
 
-BOOST_AUTO_TEST_CASE(Test_columns_store_1) {
-    std::shared_ptr<BlockStore> bstore = BlockStoreBuilder::create_memstore();
+std::unique_ptr<MetadataStorage> create_metadatastorage() {
+    // Create in-memory sqlite database.
     std::unique_ptr<MetadataStorage> meta;
     meta.reset(new MetadataStorage(":memory:"));
+    return std::move(meta);
+}
+
+std::shared_ptr<ColumnStore> create_cstore() {
+    std::shared_ptr<BlockStore> bstore = BlockStoreBuilder::create_memstore();
     std::shared_ptr<ColumnStore> cstore;
-    cstore.reset(new ColumnStore(bstore, std::move(meta)));
+    cstore.reset(new ColumnStore(bstore, create_metadatastorage()));
+    return cstore;
+}
+
+std::unique_ptr<WriteSession> create_session(std::shared_ptr<ColumnStore> cstore) {
+    std::unique_ptr<WriteSession> session;
+    session.reset(new WriteSession(cstore));
+    return session;
+}
+
+BOOST_AUTO_TEST_CASE(Test_columns_store_create_1) {
+    std::shared_ptr<ColumnStore> cstore = create_cstore();
+    std::unique_ptr<WriteSession> session = create_session(cstore);
+}
+
+BOOST_AUTO_TEST_CASE(Test_column_store_add_series_1) {
+    aku_Status status;
+    const char* sname = "hello world=1";
+    const char* end = sname + strlen(sname);
+
+    auto meta = create_metadatastorage();
+    auto bstore = BlockStoreBuilder::create_memstore();
+    auto cstore = create_cstore();
+    auto sessiona = create_session(cstore);
+    auto sessionb = create_session(cstore);
+
+    aku_Sample samplea;
+    status = sessiona->init_series_id(sname, end, &samplea);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    aku_Sample sampleb;
+    // Should initialize from global data
+    status = sessionb->init_series_id(sname, end, &sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    BOOST_REQUIRE_EQUAL(samplea.paramid, sampleb.paramid);
+
+    // Should read local data
+    status = sessionb->init_series_id(sname, end, &sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    BOOST_REQUIRE_EQUAL(samplea.paramid, sampleb.paramid);
+}
+
+BOOST_AUTO_TEST_CASE(Test_column_store_add_values_1) {
+    aku_Status status;
+    const char* sname = "hello world=1";
+    const char* end = sname + strlen(sname);
+
+    auto meta = create_metadatastorage();
+    auto bstore = BlockStoreBuilder::create_memstore();
+    auto cstore = create_cstore();
+    auto sessiona = create_session(cstore);
+    auto sessionb = create_session(cstore);
+
+    aku_Sample samplea;
+    samplea.payload.type = AKU_PAYLOAD_FLOAT;
+    samplea.timestamp = 111;
+    samplea.payload.float64 = 111;
+    status = sessiona->init_series_id(sname, end, &samplea);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+    status = sessiona->write(samplea);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    aku_Sample sampleb;
+    sampleb.payload.type = AKU_PAYLOAD_FLOAT;
+    sampleb.timestamp = 222;
+    sampleb.payload.float64 = 222;
+    // Should initialize from global data
+    status = sessionb->init_series_id(sname, end, &sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+    status = sessionb->write(sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    BOOST_REQUIRE_EQUAL(samplea.paramid, sampleb.paramid);
+
+    // Should read local data
+    sampleb.timestamp = 333;
+    sampleb.payload.float64 = 333;
+    status = sessiona->init_series_id(sname, end, &sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+    status = sessiona->write(sampleb);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+}
+
+
+BOOST_AUTO_TEST_CASE(Test_column_store_add_values_2) {
+    aku_Status status;
+    const char* sname = "hello world=1";
+    const char* end = sname + strlen(sname);
+
+    auto meta = create_metadatastorage();
+    auto bstore = BlockStoreBuilder::create_memstore();
+    auto cstore = create_cstore();
+    auto sessiona = create_session(cstore);
+    {
+        auto sessionb = create_session(cstore);
+
+        aku_Sample sample;
+        sample.payload.type = AKU_PAYLOAD_FLOAT;
+        sample.timestamp = 111;
+        sample.payload.float64 = 111;
+        status = sessionb->init_series_id(sname, end, &sample);
+        BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+        status = sessionb->write(sample);
+        BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+        // disatcher should be freed and registry entry should be returned
+    }
+    aku_Sample sample;
+    sample.payload.type = AKU_PAYLOAD_FLOAT;
+    sample.timestamp = 222;
+    sample.payload.float64 = 222;
+
+    status = sessiona->init_series_id(sname, end, &sample);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+    status = sessiona->write(sample);
+    BOOST_REQUIRE_EQUAL(status, AKU_SUCCESS);
+
+    BOOST_REQUIRE_EQUAL(sample.paramid, sample.paramid);
+}
+
+BOOST_AUTO_TEST_CASE(Test_column_store_add_values_3) {
+    aku_Status status;
+    auto meta = create_metadatastorage();
+    auto bstore = BlockStoreBuilder::create_memstore();
+    auto cstore = create_cstore();
+    auto sessiona = create_session(cstore);
+    aku_Sample sample;
+    sample.payload.type = AKU_PAYLOAD_FLOAT;
+    sample.paramid = 111;
+    sample.timestamp = 111;
+    sample.payload.float64 = 111;
+    status = sessiona->write(sample);  // series with id 111 doesn't exists
+    BOOST_REQUIRE_NE(status, AKU_SUCCESS);
 }
