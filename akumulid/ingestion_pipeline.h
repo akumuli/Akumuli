45513198b39c0ce42f3dda40e9287612b735b7c0
Eugene Lazin <4lazin@gmail.com>
2016-10-10 00:26:51 -0700

    Enabled akumulid build. Ingestion pipeline/spout removed, session should be used instead. #124

--- akumulid/ingestion_pipeline.h
+++ akumulid/ingestion_pipeline.h
@@ -82,8 +82,7 @@ private:
     aku_Database* db_;
 
 public:
-    AkumuliConnection(const char* path, bool hugetlb, Durability durability,
-                      u32 compression_threshold, u64 window_width, u64 cache_size);
+    AkumuliConnection(const char* path);
 
     virtual void close();
 
@@ -96,117 +95,6 @@ public:
     virtual aku_Status series_to_param_id(const char* name, size_t size, aku_Sample* sample);
 
     virtual std::string get_all_stats();
-};
-
-using boost::lockfree::queue;
-using boost::lockfree::capacity;
-
-
-enum BackoffPolicy {
-    AKU_THROTTLE,
-    AKU_LINEAR_BACKOFF,
-};
-
-
-//! Callback from pipeline to session
-typedef std::function<void(aku_Status, u64)> PipelineErrorCb;
-
-
-/** Pipeline's spout.
-  * Object of this class can be used to ingest data to pipeline.
-  * It should be connected with IngestionPipeline instance with
-  * the shared queue. Pooling is used to simplify allocator's
-  * life. All TVals should be deleted in the same thread where
-  * they was created. This shuld minimize contention inside
-  * allocator and limit overall memory usage (no need to create
-  * pool of objects beforehand).
-  */
-struct PipelineSpout : ProtocolConsumer {
-
-    // Constants
-    enum {
-        //! PVal pool size
-        POOL_SIZE = 0x200,
-        QCAP      = 0x10,
-    };
-
-    // Typedefs
-    typedef struct { char emptybits[64]; } Padding;  //< Padding
-    typedef std::atomic<u64> SpoutCounter;           //< Shared counter
-    typedef struct {
-        aku_Sample       sample;                      //< Value
-        SpoutCounter*    cnt;                         //< Pointer to spout's shared counter
-        PipelineErrorCb* on_error;                    //< On error callback
-    } TVal;                                           //< Value
-    typedef std::shared_ptr<TVal>         PVal;       //< Pointer to value
-    typedef queue<TVal*>                  Queue;      //< Queue class
-    typedef std::shared_ptr<Queue>        PQueue;     //< Pointer to queue
-    typedef std::shared_ptr<DbConnection> PDatabase;  //< Database "connection"
-
-    // Data
-    SpoutCounter        created_;  //< Created elements counter
-    Padding             pad0;
-    SpoutCounter        deleted_;  //< Deleted elements counter
-    std::vector<PVal>   pool_;     //< TVal pool
-    Padding             pad1;
-    PQueue              queue_;  //< Queue
-    const BackoffPolicy backoff_;
-    Logger              logger_;    //< Logger instance
-    PipelineErrorCb     on_error_;  //< Session callback
-    PDatabase           db_;
-
-    // C-tor
-    PipelineSpout(std::shared_ptr<Queue> q, BackoffPolicy bp, std::shared_ptr<DbConnection> con);
-    ~PipelineSpout();
-
-    void set_error_cb(PipelineErrorCb cb);
-
-    // ProtocolConsumer
-    virtual void write(const aku_Sample& sample);
-    virtual void add_bulk_string(const Byte* buffer, size_t n);
-
-    // Utility
-    //! Reserve index for the next TVal in the pool or negative value on error.
-    int get_index_of_empty_slot();
-
-    /** Dump all errors to ostr or report that everything is OK
-      * @param ostr stream to write
-      */
-    void get_error(std::ostream& ostr);
-
-    aku_Status series_to_param_id(const char* str, size_t strlen, aku_Sample* sample);
-
-    /** Returns true if all TVal's is processed */
-    bool is_empty() const;
-};
-
-class IngestionPipeline : public std::enable_shared_from_this<IngestionPipeline> {
-    enum {
-        N_QUEUES = 8,
-    };
-    typedef boost::barrier             Barr;
-    std::shared_ptr<DbConnection>      con_;       //< DB connection
-    std::vector<PipelineSpout::PQueue> queues_;    //< Queues collection
-    std::atomic<int>                   ixmake_;    //< Index for the make_spout mehtod
-    Barr                               stopbar_;   //< Stopping barrier
-    Barr                               startbar_;  //< Stopping barrier
-    static PipelineSpout::TVal*        POISON;     //< Poisoned object to stop worker thread
-    static int                         TIMEOUT;    //< Close timeout
-    const BackoffPolicy                backoff_;   //< Back-pressure policy
-    Logger                             logger_;    //< Logger instance
-public:
-    /** Create new pipeline topology.
-      */
-    IngestionPipeline(std::shared_ptr<DbConnection> con, BackoffPolicy bp = AKU_THROTTLE);
-
-    /** Run pipeline topology.
-      */
-    void start();
-
-    /** Add new pipeline spout. */
-    std::shared_ptr<PipelineSpout> make_spout();
-
-    void stop();
 };
 
 }  // namespace Akumuli
