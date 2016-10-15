eff0c52f0bdacd7b3e590399c6dbcb5693fda8d4
Eugene Lazin <4lazin@gmail.com>
2016-10-15 16:33:18 -0700

    Added min volume size check (1MB)

--- libakumuli/storage2.cpp
+++ libakumuli/storage2.cpp
@@ -394,9 +394,13 @@ aku_Status Storage::new_database( const char     *file_name
                                 , u64             volume_size)
 {
     // Check for max volume size
-    const u64 MAX_SIZE = 0x100000000 * 4096 - 1;
+    const u64 MAX_SIZE = 0x100000000 * 4096 - 1;  // 15TB
+    const u64 MIN_SIZE = 0x100;  // 1MB
     if (volume_size > MAX_SIZE) {
-        Logger::msg(AKU_LOG_ERROR, "Volume size is too big: " + std::to_string(volume_size));
+        Logger::msg(AKU_LOG_ERROR, "Volume size is too big: " + std::to_string(volume_size) + ", it can't be greater than 15TB");
+        return AKU_EBAD_ARG;
+    } else if (volume_size < MIN_SIZE) {
+        Logger::msg(AKU_LOG_ERROR, "Volume size is too small: " + std::to_string(volume_size) + ", it can't be less than 1MB");
         return AKU_EBAD_ARG;
     }
     // Create volumes and metapage
