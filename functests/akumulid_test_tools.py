5d292873ce8e4230bfb933e18d641c467e695cc0
Eugene Lazin <4lazin@gmail.com>
2017-01-26 16:41:10 +0300

    Volume overflow timeouts on CI (probably because space optimization)

--- functests/akumulid_test_tools.py
+++ functests/akumulid_test_tools.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import socket
 import datetime
+import random
 try:
     import ConfigParser as ini
 except ImportError:
@@ -107,6 +108,7 @@ def infinite_msg_stream(batch_size, metric_name, **kwargs):
     template = '\r\n'.join(['+{2}\r\n+{0}\r\n+{1}']*batch_size) + '\r\n'
     sseries = metric_name + ' ' + ' '.join(['{0}={1}'.format(key, val) for key, val in kwargs.iteritems()])
     while True:
+        divider = 1.0 + random.random()*10.0
         dt = datetime.datetime.utcnow()
         m = template.format(dt.strftime('%Y%m%dT%H%M%S.%f'), float(i), sseries)
         yield m
