115c11a884ec61d89ec4a66c791122b54c50d508
Eugene Lazin <4lazin@gmail.com>
2017-07-06 21:50:58 +0300

    Fix #194
    Bug in join query materializer fixed.
    Roundtrip test is improved (it checks join query results now, but the
    'expected' file is broken so it fails.

--- functests/roundtrip.sh
+++ functests/roundtrip.sh
@@ -10,6 +10,7 @@
 # Run akumulid server in the background
 ./akumulid/akumulid&
 pid=$!
+error=
 
 if [ ! -f resp_1day_1000names_10sec_step.gz ]; then
     # Download data from S3
@@ -29,6 +30,15 @@ else
     sleep 1
 fi
 
+if [ ! -f expected-join-results.resp.gz ]; then
+    # Download data from S3
+    echo "downloading expected-join-results.resp.gz ..."
+    wget -q https://s3-us-west-2.amazonaws.com/travis-ci-data/expected-join-results.resp.gz
+    echo "expected-join-results.resp.gz downloaded"
+else
+    sleep 1
+fi
+
 
 # Insert downloaded data
 echo "writing data"
@@ -41,12 +51,20 @@ curl -s --url localhost:8181/api/query -d '{ "select": "meta:names" }' > actual-
 
 if ! cmp expected-meta-results.resp actual-meta-results.resp >/dev/null 2>&1
 then
-    echo "Metadata query error"
-    exit 1
+    error="Metadata query error"
 fi
-curl -s --url localhost:8181/api/query -d '{ "join": ["cpu.user","cpu.sys","cpu.real","idle","mem.commit","mem.virt","iops","tcp.packets.in","tcp.packets.out","tcp.ret"], "range": { "from": "20170101T000000.000000", "to": "20170102T000000.000000" }}' > actual-join-results.resp
+curl -s --url localhost:8181/api/query -d '{ "join": ["cpu.user","cpu.sys","cpu.real","idle","mem.commit","mem.virt","iops","tcp.packets.in","tcp.packets.out","tcp.ret"], "range": { "from": "20170101T000000.000000", "to": "20170102T000000.000000" }}' | gzip > actual-join-results.resp.gz
 
 # TODO: check the results
+if ! diff -q <(zcat expected-join-results.resp.gz) <(zcat actual-join-results.resp.gz)
+then
+    error="Join query error, ${error}"
+fi
 
 # Stop the database server
 kill -INT ${pid}
+
+if [ -n "${error}" ]; then
+    echo "${error}"
+    exit 1
+fi
