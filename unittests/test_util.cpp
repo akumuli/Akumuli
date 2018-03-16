023bc59f4481e39c7071ec2c4def7bb04e26c9ba
Eugene Lazin <4lazin@gmail.com>
2018-03-16 12:19:16 -0700

    Remove unneded unit-test

--- unittests/test_util.cpp
+++ unittests/test_util.cpp
@@ -3,7 +3,6 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
 #include <boost/test/unit_test.hpp>
-#include <apr.h>
 
 #include "akumuli_def.h"
 #include "util.h"
@@ -18,96 +17,11 @@ void test_logger(aku_LogLevel tag, const char* msg) {
 
 struct AkumuliInitializer {
     AkumuliInitializer() {
-        apr_initialize();
         Akumuli::Logger::set_logger(&test_logger);
     }
 };
 
 AkumuliInitializer initializer;
-
-void create_tmp_file(const char* file_path, int len) {
-    apr_pool_t* pool = NULL;
-    apr_file_t* file = NULL;
-    apr_status_t status = apr_pool_create(&pool, NULL);
-    if (status == APR_SUCCESS) {
-        status = apr_file_open(&file, file_path, APR_WRITE|APR_CREATE, APR_OS_DEFAULT, pool);
-        if (status == APR_SUCCESS) {
-            status = apr_file_trunc(file, len);
-            if (status == APR_SUCCESS) {
-                status = apr_file_close(file);
-            }
-        }
-    }
-    if (pool)
-        apr_pool_destroy(pool);
-    if (status != APR_SUCCESS) {
-        BOOST_FAIL(apr_error_message(status));
-    }
-}
-
-void delete_tmp_file(const char* file_path) {
-    apr_pool_t* pool = NULL;
-    apr_pool_create(&pool, NULL);
-    apr_file_remove(file_path, pool);
-    apr_pool_destroy(pool);
-}
-
-BOOST_AUTO_TEST_CASE(TestMmap1)
-{
-    const char* tmp_file = "testfile";
-    delete_tmp_file(tmp_file);
-    create_tmp_file(tmp_file, 100);
-    MemoryMappedFile mmap(tmp_file, false);
-    BOOST_REQUIRE(mmap.is_bad() == false);
-    BOOST_REQUIRE(mmap.get_size() == 100);
-    delete_tmp_file(tmp_file);
-}
-
-BOOST_AUTO_TEST_CASE(TestMmap3)
-{
-    const char* tmp_file = "testfile";
-    delete_tmp_file(tmp_file);
-    create_tmp_file(tmp_file, 100);
-    {
-        MemoryMappedFile mmap(tmp_file, false);
-        BOOST_REQUIRE(mmap.is_bad() == false);
-        BOOST_REQUIRE(mmap.get_size() == 100);
-        char* begin = (char*)mmap.get_pointer();
-        char* end = begin + 99;
-        *begin = 42;
-        *end = 24;
-    }
-
-    {
-        MemoryMappedFile mmap(tmp_file, false);
-        BOOST_REQUIRE(mmap.is_bad() == false);
-        BOOST_REQUIRE(mmap.get_size() == 100);
-        char* begin = (char*)mmap.get_pointer();
-        char* end = begin + 99;
-        BOOST_REQUIRE(*begin == 42);
-        BOOST_REQUIRE(*end == 24);
-    }
-
-    delete_tmp_file(tmp_file);
-}
-
-BOOST_AUTO_TEST_CASE(TestMmap4)
-{
-    const char* tmp_file = "testfile";
-    delete_tmp_file(tmp_file);
-    create_tmp_file(tmp_file, 100);
-    {
-        MemoryMappedFile mmap(tmp_file, false);
-        BOOST_REQUIRE(mmap.is_bad() == false);
-        BOOST_REQUIRE(mmap.get_size() == 100);
-        char* begin = (char*)mmap.get_pointer();
-        char* end = begin + 99;
-        *begin = 42;
-        *end = 24;
-    }
-
-    delete_tmp_file(tmp_file);
-}
 
 BOOST_AUTO_TEST_CASE(test_crc32c_0) {
     auto crc32hw = chose_crc32c_implementation(CRC32C_hint::DETECT);
