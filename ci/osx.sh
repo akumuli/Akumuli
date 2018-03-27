ee25e6169493c78db5961e6ce3f58e5ea484d3ef
Eugene Lazin <4lazin@gmail.com>
2018-03-27 11:28:07 -0700

    Update CI scripts

---
+++ ci/osx.sh
@@ -0,0 +1,15 @@
+functests/storage_test /tmp
+akumulid/akumulid --init
+python functests/akumulid_test_tools.py set_log_path $HOME/akumuli.log
+python functests/test_data_ingestion.py akumulid/ TCP
+python functests/test_data_ingestion.py akumulid/ UDP
+python functests/test_data_ingestion_bulk.py akumulid/ TCP
+python functests/test_data_ingestion_bulk.py akumulid/ UDP
+python functests/test_query_language.py akumulid/
+python functests/test_volume_overflow.py akumulid/
+python functests/test_database_overflow.py akumulid/
+python functests/test_restart.py akumulid/
+python functests/test_kill.py akumulid/
+python functests/test_ingestion_errors.py akumulid/
+python functests/test_join_query.py akumulid/
+bash functests/roundtrip.sh
