be036a49408be3fe7e28f6c2cf549f3df5585dc9
Eugene Lazin <4lazin@gmail.com>
2018-05-30 15:27:32 -0700

    Testing deployment

--- CI/runci.sh
+++ CI/runci.sh
@@ -7,21 +7,29 @@ sh ./CI/prerequisites-$1.sh
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-if [ $? -ne 0 ]; then
-    exit 1
+
+if [[ $GENERIC_BUILD == true ]]; then
+    cmake .. -DCMAKE_BUILD_TYPE=ReleaseGen;
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+else
+    cmake .. -DCMAKE_BUILD_TYPE=Release;
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 fi
 
-make akumulid -j4
+make -j4
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 # Run tests
-#bash ./CI/test-$1.sh
-#if [ $? -ne 0 ]; then
-#    exit 1
-#fi
+bash ./CI/test-$1.sh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 make install
 if [ $? -ne 0 ]; then
