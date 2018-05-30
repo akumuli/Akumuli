c15510b0e7f8a37507cebc01f6e480351e889bac
Eugene Lazin <4lazin@gmail.com>
2018-05-30 12:46:13 -0700

    Try to fix docker.sh

--- CI/docker.sh
+++ CI/docker.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-echo "Running docker.sh script for $TRAVIS_OS_NAME"
+echo "Running docker.sh script for $1"
 echo "Work dir: $(pwd)"
 
 apt-get update
-sh prerequisites.sh
+sh prerequisites-$1.sh
 
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 make -j4
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 ctest -VV
+if [ $? -ne 0 ]; then
+    exit 1
+fi
--- prerequisites.sh
+++ prerequisites.sh
@@ -3,7 +3,7 @@
 distri=$(awk '/^DISTRIB_ID=|^ID=/' /etc/*-release | sed 's/.*=//' | sed 's/"//g' | tr '[:upper:]' '[:lower:]' | head -n1)
 
 if [ "x$distri" = "x" ]; then
-	distri=$(find /etc/*-release | head -n1 | xargs grep -o '(\w*)' | sed 's/[()]//g' | tr '[:upper:]' '[:lower:]')
+        distri=$(find /etc/*-release | head -n1 | xargs grep -o '(\w*)' | sed 's/[()]//g' | tr '[:upper:]' '[:lower:]')
 fi
 
 case $distri in
@@ -18,8 +18,8 @@ esac
 
 
 if [ "x$pkgman" = "xyum" -o "x$pkgman" = "xdnf" ]; then
-	echo "Building for Centos/Fedora/RHEL"
-        $pkgman install -y cmake \
+        echo "Building for Centos/Fedora/RHEL"
+        sudo $pkgman install -y cmake \
                           boost boost-devel \
                           log4cxx log4cxx-devel \
                           sqlite sqlite-devel \
@@ -27,23 +27,24 @@ if [ "x$pkgman" = "xyum" -o "x$pkgman" = "xdnf" ]; then
                           libmicrohttpd-devel \
                           jemalloc-devel
 else
-	if [ "x$pkgman" = "xapt" ]; then
-                echo 'The script will install packages using apt-get.'
-		     
-		echo 'Trying to install boost libraries'
-                apt-get install -y libboost-all-dev
+        if [ "x$pkgman" = "xapt" ]; then
+                echo 'The script will install packages using apt-get.' \
+                     'It can ask for your sudo password.'
 
-		echo 'Trying to install other libraries'
-        apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
-        apt-get install -y liblog4cxx10-dev liblog4cxx10
-        apt-get install -y libjemalloc-dev
-        apt-get install -y libsqlite3-dev
-        apt-get install -y libmicrohttpd-dev
+                echo 'Trying to install boost libraries'
+                sudo apt-get install libboost-all-dev
 
-		echo 'Trying to install cmake'
-                apt-get install -y cmake
-	else
-		echo "ERROR: Unknown package manager: $distri"
-		exit 1
-	fi
+                echo 'Trying to install other libraries'
+        sudo apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
+        sudo apt-get install -y liblog4cxx10-dev liblog4cxx10
+        sudo apt-get install -y libjemalloc-dev
+        sudo apt-get install -y libsqlite3-dev
+        sudo apt-get install -y libmicrohttpd-dev
+
+                echo 'Trying to install cmake'
+                sudo apt-get install -y cmake
+        else
+                echo "ERROR: Unknown package manager: $distri"
+                exit 1
+        fi
 fi #package manager check
