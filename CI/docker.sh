212c7ad38462fbbf9160db53e76225d349b81287
Eugene Lazin <4lazin@gmail.com>
2018-05-30 07:43:12 -0700

    Remove sudo from docker.sh

--- CI/docker.sh
+++ CI/docker.sh
@@ -3,8 +3,8 @@
 echo "Running docker.sh script for $TRAVIS_OS_NAME" > docker.out
 echo "Work dir: $(pwd)" >> docker.out
 
-sudo apt-get update >> docker.out 2>&1
-sudo sh prerequisites.sh >> docker.out 2>&1
+apt-get update >> docker.out 2>&1
+sh prerequisites.sh >> docker.out 2>&1
 
 mkdir build
 cd build
