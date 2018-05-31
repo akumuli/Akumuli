22e6e89cde8d3c7baaf3c85e303ebe8453e91d58
Eugene Lazin <4lazin@gmail.com>
2018-05-31 08:41:56 -0700

    Add debian build

---
+++ CI/prerequisites-debian-stretch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get update
+apt-get install -y libboost-all-dev
+apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
+apt-get install -y liblog4cxx10-dev liblog4cxx10v5
+apt-get install -y libjemalloc-dev
+apt-get install -y libsqlite3-dev
+apt-get install -y libmicrohttpd-dev
+apt-get install -y cmake
