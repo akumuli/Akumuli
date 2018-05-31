a42f5132cbc846568b523f12b3847506b181bc88
Eugene Lazin <4lazin@gmail.com>
2018-05-31 06:34:44 -0700

    Trying to fix docker build

--- CI/prepare-docker-image.sh
+++ CI/prepare-docker-image.sh
@@ -15,7 +15,13 @@ if [ $? -ne 0 ]; then
 fi
 cp akumuli_*_amd64.deb ./docker;
 export VERSION=`ls akumuli_*_amd64.deb | sed -n 's/akumuli_\([0-9].[0-9].[0-9][0-9]\)-1ubuntu1.0_amd64\.deb/\1/p'`
-export REPO=`if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then echo "akumuli/akumuli"; else echo "akumuli/test"; fi`;
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]];  then
+    # master branch build goes to main repo
+    export REPO="akumuli/akumuli"
+else
+    # pull requests and other builds goes to test
+    export REPO="akumuli/test"
+fi
 export TAG=`if [[ $GENERIC_BUILD == "false" ]]; then echo "skylake"; else echo "generic"; fi`;
 docker build -t $REPO:$VERSION-$TAG ./docker;
 if [ $? -ne 0 ]; then
@@ -23,3 +29,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Version: $VERSION"
+echo "Docker repository: $REPO"
+echo "Docker tag: $TAG"
