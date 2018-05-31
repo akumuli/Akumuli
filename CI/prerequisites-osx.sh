fd4af4cd03d222e4c64300eafaa52bd54e553f36
Eugene Lazin <4lazin@gmail.com>
2018-05-31 03:50:12 -0700

    Add OSX, fix travis.yml

---
+++ CI/prerequisites-osx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+brew update
+brew install cmake
+brew install boost
+brew install log4cxx
+brew install jemalloc
+brew install sqlite
+brew install libmicrohttpd
+brew install apr
+brew install apr-util
---
+++ CI/test-osx.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "Running tests for $1"
+echo "Work dir: $(pwd)"
+
+echo "Running unit-tests"
+ctest -VV
+if [ $? -ne 0 ]; then
+    # We have to manually check the exit code since
+    # Travis-CI doesn't work well with `set -e`.
+    echo "Unit-tests failed" >&2
+    exit 1
+fi
+
+functests/storage_test /tmp;
+if [ $? -ne 0 ]; then
+    echo "API test failed" >&2
+    exit 1
+fi
+
+echo "Set up disk constrained environment"
+akumulid/akumulid --init
+python functests/akumulid_test_tools.py set_log_path $HOME/akumuli.log
+
+echo "Running base integration tests"
+python functests/test_data_ingestion.py akumulid/ TCP
+if [ $? -ne 0 ]; then
+    echo "Base test failed" >&2
+    exit 1
+fi
+python functests/test_data_ingestion.py akumulid/ UDP
+if [ $? -ne 0 ]; then
+    echo "Base test failed" >&2
+    exit 1
+fi
+python functests/test_data_ingestion_bulk.py akumulid/ TCP
+if [ $? -ne 0 ]; then
+    echo "Base test failed" >&2
+    exit 1
+fi
+python functests/test_data_ingestion_bulk.py akumulid/ UDP
+if [ $? -ne 0 ]; then
+    echo "Base test failed" >&2
+    exit 1
+fi
