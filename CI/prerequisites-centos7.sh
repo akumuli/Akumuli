da52110e47ab8765e95f9041aef8398761d0c66f
Eugene Lazin <4lazin@gmail.com>
2018-06-01 06:18:55 -0700

    Install rpmbuild in centos7

--- CI/prerequisites-centos7.sh
+++ CI/prerequisites-centos7.sh
@@ -3,6 +3,7 @@
 echo "Building for Centos/Fedora/RHEL"
 yum update -y
 yum install -y gcc gcc-c++ make
+yum install -y rpm-build
 yum install -y boost-devel
 yum install -y apr-devel apr-util-devel apr-util-sqlite
 yum install -y log4cxx log4cxx-devel
