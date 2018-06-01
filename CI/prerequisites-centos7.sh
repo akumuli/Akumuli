c454e6e45d0d8fdd9b5a756f87d93d45cbb2a541
Eugene Lazin <4lazin@gmail.com>
2018-06-01 00:15:06 -0700

    Fix centos dependencies

--- CI/prerequisites-centos7.sh
+++ CI/prerequisites-centos7.sh
@@ -6,7 +6,7 @@ yum install -y gcc gcc-c++ make
 yum install -y boost-devel
 yum install -y apr-devel apr-util-devel apr-util-sqlite
 yum install -y log4cxx log4cxx-devel
-yum install -y jemalloc-devel
+yum install -y jemalloc jemalloc-devel
 yum install -y sqlite sqlite-devel
-yum install -y libmicrohttpd-devel
+yum install -y libmicrohttpd libmicrohttpd-devel
 yum install -y cmake
