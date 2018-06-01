340af82ca480bd03af5f81bd4a3ad027eef6c721
Eugene Lazin <4lazin@gmail.com>
2018-05-31 23:57:44 -0700

    Install make on centos

--- CI/prerequisites-centos7.sh
+++ CI/prerequisites-centos7.sh
@@ -2,6 +2,7 @@
 
 echo "Building for Centos/Fedora/RHEL"
 yum update -y
+yum install -y gcc gcc-c++ make
 yum install -y boost-devel
 yum install -y apr-devel apr-util-devel apr-util-sqlite
 yum install -y log4cxx log4cxx-devel
