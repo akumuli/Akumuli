55ab1e275f4301be24b76fef4840dd6819126b38
Eugene Lazin <4lazin@gmail.com>
2019-02-13 16:15:56 +0300

    Fix bionic build

--- CI/prerequisites-ubuntu-18.04.sh
+++ CI/prerequisites-ubuntu-18.04.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y sudo
 apt-get install -y libboost-all-dev
 apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
-apt-get install -y liblog4cxx10-dev liblog4cxx10v5
+apt-get install -y liblog4cxx-dev
 apt-get install -y libjemalloc-dev
 apt-get install -y libsqlite3-dev
 apt-get install -y libmicrohttpd-dev
