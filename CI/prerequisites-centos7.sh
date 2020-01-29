baf6cfa7b379912dced336cad018482c9a8ef7e6
Eugene Lazin <4lazin@gmail.com>
2020-01-29 15:39:36 +0300

    Move scl_enable to the begining of the script

--- CI/prerequisites-centos7.sh
+++ CI/prerequisites-centos7.sh
@@ -3,6 +3,9 @@
 echo "Building for Centos/Fedora/RHEL"
 yum install -y epel-release
 yum update  -y
+yum install -y centos-release-scl
+yum install -y devtoolset-7
+source scl_source enable devtoolset-7
 yum install -y rpm-build
 yum install -y boost-devel
 yum install -y apr-devel apr-util-devel apr-util-sqlite
@@ -11,8 +14,3 @@ yum install -y jemalloc jemalloc-devel
 yum install -y sqlite sqlite-devel
 yum install -y libmicrohttpd libmicrohttpd-devel
 yum install -y cmake
-yum install -y centos-release-scl
-yum install -y devtoolset-7
-echo "Enable devtoolset-7"
-source scl_source enable devtoolset-7
-echo "Finished"
