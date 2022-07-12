39715cc77c6f54fd5eae44d702a24a1297e6b877
Hamid EL MAAZOUZ <hamid.elmaazouz@gmail.com>
2022-07-12 20:08:37 +0100

    refactor: fix indentation

--- prerequisites.sh
+++ prerequisites.sh
@@ -35,12 +35,12 @@ else
                 sudo apt-get install libboost-all-dev
 
                 echo 'Trying to install other libraries'
-        sudo apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
-        sudo apt-get install -y liblog4cxx10-dev liblog4cxx10
-        sudo apt-get install -y liblog4cxx-dev
-        sudo apt-get install -y libjemalloc-dev
-        sudo apt-get install -y libsqlite3-dev
-        sudo apt-get install -y libmicrohttpd-dev
+                sudo apt-get install -y libapr1-dev libaprutil1-dev libaprutil1-dbd-sqlite3
+                sudo apt-get install -y liblog4cxx10-dev liblog4cxx10
+                sudo apt-get install -y liblog4cxx-dev
+                sudo apt-get install -y libjemalloc-dev
+                sudo apt-get install -y libsqlite3-dev
+                sudo apt-get install -y libmicrohttpd-dev
 
                 echo 'Trying to install cmake'
                 sudo apt-get install -y cmake
